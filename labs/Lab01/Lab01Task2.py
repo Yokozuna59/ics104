cc06c0ce37c4f4ce4e7d2347bd900e27a3c8a282
Yokozuna59 <u.yokozuna@gmail.com>
2022-06-07 19:58:12 +0300

    push lab 1 answers

---
+++ labs/Lab01/Lab01Task2.py
@@ -0,0 +1,6 @@
+# Setting the Celsius value first
+Celsius = 100
+
+# Write your code below this line
+Fahrenheit = 32 + 9/5 * Celsius
+print("The", Celsius, "Celsius is", Fahrenheit, "in Fahrenheit.")
---
+++ labs/Lab01/Lab01Task3.py
@@ -0,0 +1,10 @@
+# Defining the constant PI
+PI = 3.14159
+
+# Setting the value of the radius
+radius = 10
+
+# Write your code below this line
+Area = PI * radius * radius
+circumference = 2 * PI * radius
+print("The area is", Area, "units squere and the circumfernce is", circumference, "units, for the radius of 10 units.")

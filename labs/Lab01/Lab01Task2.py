d2342676d202d6391ebbcee8a847dbe86276427a
Yokozuna59 <u.yokozuna@gmail.com>
2022-06-15 21:29:57 +0300

    add my solution to the code

--- labs/Lab01/Lab01Task2.py
+++ labs/Lab01/Lab01Task2.py
@@ -2,9 +2,36 @@
 # This program converts temperature from Celsius into Fahrenheit.
 #
 
+# Write your code below this line
+
+#%% My code from what I learned in class
+
 # Setting the Celsius value first
 celsius = 100
 
-# Write your code below this line
-fahrenheit = (9 * celsius)/5 + 32 # Calculate the temperature from Celsius into Fahrenheit
+# Calculate the temperature from Celsius into Fahrenheit
+fahrenheit = (9 * celsius)/5 + 32
+
+# Print the result
 print("The", celsius, "Celsius is", fahrenheit, "in Fahrenheit.")
+
+#################################################################################
+#%% My code from what I learned outside of class
+
+# Ask the user to enter the temperature in Celsius
+user_celsius = input("Enter the temperature in Celsius: ")
+
+# Check if user has enter a float numbet
+try:
+    # If he did so, convert it to float
+    float_celsius=float(user_celsius)
+except ValueError:
+    # If he did not, print an error message and exit the program
+    print("You have entered an invalid value.")
+    exit()
+
+# Calculate the temperature from Celsius into Fahrenheit
+convert_to_fahrenheit = (float_celsius * 9)/5 + 32
+
+# Print the result
+print("The %.2f Celsius is %.2f Fahrenheit." %(float_celsius, convert_to_fahrenheit))
--- labs/Lab01/Lab01Task3.py
+++ labs/Lab01/Lab01Task3.py
@@ -2,13 +2,48 @@
 # This program calculate the area and circumference.
 #
 
+# Write your code below this line
+
+#%% My code from what I learned in class
+
 # Defining the constant PI
 PI = 3.14159
 
 # Setting the value of the radius
 radius = 10
 
-# Write your code below this line
-area = PI * radius * radius # Calculate the area in unit square
-circumference = 2 * PI * radius # Calculate the circumference in unit
-print("The area is", area, "units square and circumference is", circumference, "units, of a 10 units radius.")
+# Calculate the area in unit square
+area = PI * radius * radius
+
+# Calculate the circumference in unit
+circumference = 2 * PI * radius
+
+# Print the result
+print("The area is", area, "units square and circumference is", circumference, "units, of the 10 units radius.")
+
+#####################################################################################################################
+#%% My code from what I learned outside of class
+
+# import pi as PI from math module
+from math import pi as PI
+
+# Ask the user to enter the radius
+user_radius = input("Enter the radius of the circle: ")
+
+# Check if user has enter a float numbet
+try:
+    # If he did so, convert it to float
+    float_radius=float(user_radius)
+except ValueError:
+    # If he did not, print an error message and exit the program
+    print("You have entered an invalid value.")
+    exit()
+
+# Calculate the area in unit square
+area = PI * float_radius ** 2
+
+# Calculate the circumference in unit
+circumference = 2 * PI * float_radius
+
+# Print the result
+print("The area is %.2f units square and circumference is %.2f units, of the 10 units radius." %(area, circumference))
